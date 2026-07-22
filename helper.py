91b67ac3ee5d2b49ede914978071f29b12c4d39a
Mubtasim36 <22-49002-3@student.aiub.edu>
2026-07-22 11:12:36 +0600

    Learning Beginning

---
+++ helper.py
@@ -0,0 +1,3 @@
+#This file is used to store helper functions that are used in multiple files.
+def greet():
+    print("This line is from the Helper.py file: Hello! Welcome to the program.")
---
+++ main.py
@@ -0,0 +1,36 @@
+print("Hehe")
+print("Hello, World!")
+
+#Input Tutorial
+input_tutorial=input("Enter anything: ")
+print("You entered: ", input_tutorial)
+
+newinput=input("Enter name: ")
+print(f"Hello,{newinput}!")
+
+#Input and Basic calculation
+num1=input("Enter first number: ")
+num2=input("Enter second number: ")
+
+print("Summation of the numbers is: ", int(num1)+int(num2))
+print("Subtraction of the numbers is: ", int(num1)-int(num2))
+print("Multiplication of the numbers is: ", int(num1)*int(num2))
+print("Division of the numbers is: ", int(num1)/int(num2))
+
+
+#Other Calculations
+print("Integer Division of the numbers is: ", int(num1)//int(num2))
+print("Square of the numbers is: ", int(num1)**int(num2))
+
+
+#Value assign and calculation
+height=50
+width=23
+print("Summation of the numbers is: ", int(height)+int(width))
+print("Subtraction of the numbers is: ", int(height)-int(width))
+print("Multiplication of the numbers is: ", int(height)*int(width))
+print("Division of the numbers is: ", int(height)/int(width))
+
+#Importing from other files
+from helper import greet
+greet()
